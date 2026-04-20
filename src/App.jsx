3b6f38bfe6fa26e01dc5a97eb21f033e8ef1f519
Brian Zhang <zhang.15977@osu.edu>
2026-04-20 17:40:13 -0400

    implment general hero card
    add css for hero card

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ import Experience from "./components/Experience"
 import './App.css'
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({ 
+      name: "john doe",
+      title: "placeholder",
+      description: "Lorem ipsum dolor...",
+      email: "placeholder@email.com",
+      phone: "123-456-7890",
+  });
+
   return (
     <>
-      <General />
+      <General info={generalInfo} setInfo={setGeneralInfo} />
       <Education />
       <Experience />
     </>
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,7 +1,27 @@
-import "../styles/General.module.css"
+import styles from "../styles/General.module.css"
 
-export default function General() {
+export default function General({info, setInfo}) {
     return(
-        <></>
+        <header className={styles.hero}>
+            <div className={styles.header}>
+                <h2>{info.name} | {info.title}</h2>
+            </div>
+
+            <hr className={styles.divider} />
+
+            <p className={styles.description}>{info.description}</p>
+
+            <address className={styles.contact}>
+                <div className={styles.contactItem}>
+                    <img className={styles.icon} alt="phone icon" />
+                    <span>{info.phone}</span>
+                </div>
+
+                <div className={styles.contactItem}>
+                    <img className={styles.icon} alt="email icon" />
+                    <span>{info.email}</span>
+                </div>
+            </address>
+        </header>
     )
 }
